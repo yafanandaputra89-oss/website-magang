c1da5da772a44b049a3573169a4c61be184ae45a
Yafa Putra <yafaputra94@gmail.com>
2026-07-05 00:16:35 +0700

    aa

--- backend/routes/Pengajuandosenroutes.js
+++ backend/routes/Pengajuandosenroutes.js
@@ -1,21 +1,21 @@
 // routes/pengajuanDosenRoutes.js
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../controllers/pengajuanDosenController");
+const pengajuanDosenController = require("../controllers/pengajuandosencontroller");
 const { verifyToken, checkRole } = require("../middleware/authMiddleware");
 
 // MAHASISWA
-router.post("/", verifyToken, checkRole("mahasiswa"), ctrl.buatPengajuan);
-router.get("/saya", verifyToken, checkRole("mahasiswa"), ctrl.getPengajuanSaya);
+router.post("/", verifyToken, checkRole("mahasiswa"), pengajuanDosenController.buatPengajuan);
+router.get("/saya", verifyToken, checkRole("mahasiswa"), pengajuanDosenController.getPengajuanSaya);
 
 // DOSEN
-router.get("/dosen/permohonan", verifyToken, checkRole("dosen"), ctrl.getPermohonanDosen);
-router.patch("/:id/setujui", verifyToken, checkRole("dosen"), ctrl.setujuiPermohonan);
-router.patch("/:id/tolak", verifyToken, checkRole("dosen"), ctrl.tolakPermohonan);
+router.get("/dosen/permohonan", verifyToken, checkRole("dosen"), pengajuanDosenController.getPermohonanDosen);
+router.patch("/:id/setujui", verifyToken, checkRole("dosen"), pengajuanDosenController.setujuiPermohonan);
+router.patch("/:id/tolak", verifyToken, checkRole("dosen"), pengajuanDosenController.tolakPermohonan);
 
 // ADMIN PRODI
-router.get("/", verifyToken, checkRole("admin"), ctrl.getAllPengajuan);
-router.patch("/:id/tetapkan", verifyToken, checkRole("admin"), ctrl.tetapkanDosen);
-router.patch("/:id/sahkan", verifyToken, checkRole("admin"), ctrl.sahkanBimbingan); // NEW: pengesahan setelah dosen setuju usulan mahasiswa
+router.get("/", verifyToken, checkRole("admin"), pengajuanDosenController.getAllPengajuan);
+router.patch("/:id/tetapkan", verifyToken, checkRole("admin"), pengajuanDosenController.tetapkanDosen);
+router.patch("/:id/sahkan", verifyToken, checkRole("admin"), pengajuanDosenController.sahkanBimbingan); // NEW: pengesahan setelah dosen setuju usulan mahasiswa
 
 module.exports = router;
